51dd3d671ea31e613f8075528f264b790304b8c6
ihorman <ihorman@gmail.com>
2014-08-07 11:26:19 +0200

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -1,7 +1,8 @@
 include os output;
 
 AS_ADMIN="/opt/repo/versions/4.0/bin/asadmin";
-EMPTY_PASS='admin;{SSHA256}DdW1VfFCD0AqbQFzsu6Swqel1g1gZZ6f1m87JX6FQYSpu1X/BxTX5A==;asadmin';
+EMPTY_PASS='userproto;{SSHA256}DdW1VfFCD0AqbQFzsu6Swqel1g1gZZ6f1m87JX6FQYSpu1X/BxTX5A==;asadmin';
+PASS_FILE="/opt/repo/.gfpass";
 
 function _setPassword() {
 
@@ -19,9 +20,14 @@ function _setPassword() {
         }
 
         admin_key_file="/opt/repo/versions/4.0/glassfish/domains/domain1/config/admin-keyfile";
-        echo $EMPTY_PASS > $admin_key_file;
+        echo $J_OPENSHIFT_APP_ADM_PASSWORD > $PASS_FILE;
+        echo $EMPTY_PASS | sed 's/userproto/admin/g' > $admin_key_file;
+        echo $EMPTY_PASS | sed 's/userproto/jelastic/g' >> $admin_key_file;
+        
 
-        echo -e "AS_ADMIN_PASSWORD=\nAS_ADMIN_NEWPASSWORD=$password" >> "/tmp/$$";
+        echo -e "AS_ADMIN_PASSWORD=\nAS_ADMIN_NEWPASSWORD=$J_OPENSHIFT_APP_ADM_PASSWORD" >> "/tmp/$$";
         $AS_ADMIN -u admin -W "/tmp/$$" change-admin-password > /dev/null 2>&1;
+        $AS_ADMIN -u jelastic -W "/tmp/$$" change-admin-password > /dev/null 2>&1;
+        $AS_ADMIN -u admin -W "/tmp/$$" enable-secure-admin > /dev/null 2>&1;
         rm "/tmp/$$" 2>&1;
 }
