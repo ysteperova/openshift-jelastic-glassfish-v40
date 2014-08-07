f797129f231bda6be2d872d4180213e8a6c60724
ihorman <ihorman@gmail.com>
2014-08-07 11:36:22 +0200

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-# Simple deploy and undeploy scenarios for GlassFish4
+# Simple deploy and undeploy scenarios for Jetty9
 
 WGET=$(which wget);
 AS_ADMIN="/opt/repo/versions/4.0/bin/asadmin";
+PASS_FILE="/opt/repo/.gfpass";
+ADMIN_USER="jelastic";
 include output;
+
+function _runAsadminCmd(){
+        local cmd=$@;
+        [ -f $PASS_FILE ] && {
+                local temp_file=`mktemp /tmp/pass.XXXXXXX`;
+                echo -n "AS_ADMIN_PASSWORD=" > $temp_file;
+                cat $PASS_FILE >> $temp_file;
+                $AS_ADMIN -u $ADMIN_USER -W $temp_file $cmd > /dev/null 2>&1;
+                rm $temp_file;
+        } || { exit 2; };
+}
 
 function _deploy(){
      [ "x${context}" == "xROOT" ] && deploy_context="/" || deploy_context=$context;
      [ -f "${WEBROOT}/${context}.war" ] &&  rm -f "${WEBROOT}/${context}.war";
      $WGET --no-check-certificate --content-disposition -O "/tmp/${context}.war" "$package_url";
-     $AS_ADMIN  deploy --force  --contextroot "$deploy_context" "/tmp/${context}.war" >> $ACTIONS_LOG 2>&1;
+     _runAsadminCmd  deploy --force   --contextroot "$deploy_context" "/tmp/${context}.war" >> $ACTIONS_LOG 2>&1;
      local result=$?;
      [ -f "/tmp/${context}.war" ] && rm "/tmp/${context}.war";
      return $result;
 }
 
 function _undeploy(){
-     [ "x${context}" == "xROOT" ] && context="root";
-     $AS_ADMIN  undeploy   "$context"  >> $ACTIONS_LOG 2>&1;
+     [ "x${context}" == "xROOT" ] && context="/";
+     _runAsadminCmd  undeploy   "$context"  >> $ACTIONS_LOG 2>&1;
 }
+
+
+
+
+
+
+
